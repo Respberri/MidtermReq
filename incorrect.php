76c8361b42a731057c8fcd3382d4ba2bba48903a
Kurt Valdecanas <ktav3316@gmail.com>
2024-10-18 21:23:32 +0800

    credentialfix

--- incorrect.php
+++ incorrect.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invalid Credentials</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>wrong credentials, buddy</h1>
+    <div class="containerin">
+        <h1>Not Properly Logged In!</h1>
+        <form action="login.html" method="post">
+        <p>Please input the proper credentials.</p>
+
+    <p class="or">
+            -----------------
+    </p>
+    <input type="submit" class="btn" value="Retry">
+    </form>
+    </div>
 </body>
 
 </html>
