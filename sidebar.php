40c76464d2bc0158ae0e37ad46264c181bb6d939
tebsss17 <137368444+tebsss17@users.noreply.github.com>
2024-12-07 16:25:06 +0800

    modified jquery to function with no internet

--- sidebar.php
+++ sidebar.php
@@ -97,7 +97,7 @@
     </div>
 
     <!-- jquery for sub-menu toggle -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="/js/jquery.js"></script>
     <script>
         $(document).ready(function(){
             $('.sub-btn').click(function(){
