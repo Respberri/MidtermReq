6713b32c0245020125989ab476cc113f0b0ea359
lhyvi <36299204+lhyvi@users.noreply.github.com>
2024-12-08 23:16:31 +0800

    Fix bug

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@ $_debug = false;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="/font-awesome/all.min.css">
+    <link rel="stylesheet" href="/font-awesome/css/all.css">
     <script src="/js/chart.js"></script>
 </head>
 <body>
